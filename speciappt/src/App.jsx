51f1a67adfa9c041de0fcf3727e89b850d6dcfdd
joykara <joykarani1@gmail.com>
2024-01-30 23:02:53 +0300

    npm i date-fns

--- speciappt/src/App.jsx
+++ speciappt/src/App.jsx
@@ -1,14 +1,21 @@
-import { useState } from 'react'
-
-import './App.css'
+import React from 'react';
+import { BrowserRouter, Route } from 'react-router-dom';
+import ScrollToTop from './ScrollToTop';
+import Hospitals from './routes/Hospitals.jsx';
+import HospitalPage from './routes/HospitalPage.jsx';
+import BookingPage from './routes/BookingPage.jsx';
+import Navbar from './components/Navbar.jsx';
+import './App.css';
 
 function App() {
+  <BrowserRouter basename={process.env.PUBLIC_URL}>
+    <ScrollToTop>
+      <Route path="/" component={Navbar} />
+      <Route path="/" exact component={Hospitals} />
+      <Route path="/hospital-page/:hospitalId" exact component={HospitalPage} />
+      <Route path="/hospital-page/:hospitalId/:doctorId" exact component={BookingPage} />
+    </ScrollToTop>
+  </BrowserRouter>
+};
 
-  return (
-    <>
-      
-    </>
-  )
-}
-
-export default App
+export default App;
